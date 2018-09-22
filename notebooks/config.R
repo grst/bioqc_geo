67df5e4964ea834671688a22de7e1eb242fef04a
Gregor Sturm <mail@gregor-sturm.de>
2018-09-22 08:24:49 +0000

    fix complex to real TAU

--- notebooks/config.R
+++ notebooks/config.R
@@ -1,2 +1,2 @@
 FDR_THRES = 0.01 # overall false-discovery rate
-TAU = polyroot(c(FDR_THRES, -2, 1))[1]  # see 'heterogeneity_test.Rmd'. FDR cutoff for the individual tests.
+TAU = Re(polyroot(c(FDR_THRES, -2, 1))[1])  # see 'heterogeneity_test.Rmd'. FDR cutoff for the individual tests.
