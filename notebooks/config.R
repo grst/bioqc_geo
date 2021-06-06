134ac17ecfbbe2625ad0c35587fbc041bdb5f107
Gregor Sturm <mail@gregor-sturm.de>
2021-06-06 12:17:52 +0200

    Switch to one-step approach for calling heterogeneity

--- notebooks/config.R
+++ notebooks/config.R
@@ -1,3 +1,3 @@
 FDR_THRES = 0.01 # overall false-discovery rate
-TAU = Re(polyroot(c(FDR_THRES, -2, 1))[1])  # see 'heterogeneity_test.Rmd'. FDR cutoff for the individual tests.
+SLOPE_THRES = 0.01 # exclude comparisons with a slope > 0.01
 SEVERE_THRES = 0.05 # samples that don't have their reference signature enriched with a pvalue < SEVERE_THRES are eligible for "severe contamination"
